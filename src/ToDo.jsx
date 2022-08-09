f4c0cc6581fdc2a6b3c6a35fea2c563bc46fdcd4
herold1989 <jens_herold@gmx.net>
2022-08-09 15:31:09 +0200

    completing firebase integration and functionality

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FaRegTrashAlt } from 'react-icons/fa';
 
-const style = {}
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items-center`,
+};
 
-const ToDo = ({todo}) => {
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
   return (
-        <li className={style.li}>
-          <div className={style.row}>
-            <input type="checkbox" />
-            <p className={style.text}>{todo}</p>
-          </div>
-        </li>
-    )
-}
+    <li className={todo.completed ? style.liComplete : style.li}>
+      <div className={style.row}>
+        <input onChange={() => toggleComplete(todo)} type='checkbox' checked={todo.completed ? 'checked' : ''} />
+        <p onClick={() => toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>
+          {todo.text}
+        </p>
+      </div>
+      <button onClick={() => deleteTodo(todo.id)}>{<FaRegTrashAlt />}</button>
+    </li>
+  );
+};
 
-export default ToDo
+export default Todo;
